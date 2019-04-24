676c9240cb4c5e8d8975115ab1ac4cda2bd30519
wonter <100wonter@gmail.com>
2019-04-24 13:14:04 +0800

    another solution for problem 1029

--- 1029.two-city-scheduling.cpp
+++ 1029.two-city-scheduling.cpp
@@ -5,23 +5,45 @@
  */
 class Solution {
 public:
-    int twoCitySchedCost(vector<vector<int>>& costs) {
-        int n = costs.size();
-        int dp[n + 1][n + 1];
-        memset(dp, 127, sizeof(dp));
 
-        dp[0][0] = 0;
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 0; j <= i; ++j) {
-                if (j == 0) {
-                    dp[i][j] = dp[i - 1][j] + costs[i - 1][1];
-                } else {
-                    dp[i][j] = min(dp[i - 1][j - 1] + costs[i - 1][0], dp[i - 1][j] + costs[i - 1][1]);
-                }
+    // solution 1
+        // int twoCitySchedCost(vector<vector<int>>& costs) {
+        //     int n = costs.size();
+        //     int dp[n + 1][n + 1];
+        //     memset(dp, 127, sizeof(dp));
+
+        //     dp[0][0] = 0;
+        //     for (int i = 1; i <= n; ++i) {
+        //         for (int j = 0; j <= i; ++j) {
+        //             if (j == 0) {
+        //                 dp[i][j] = dp[i - 1][j] + costs[i - 1][1];
+        //             } else {
+        //                 dp[i][j] = min(dp[i - 1][j - 1] + costs[i - 1][0], dp[i - 1][j] + costs[i - 1][1]);
+        //             }
+        //         }
+        //     }
+
+        //     return dp[n][n / 2];
+        // }
+
+    // solution 2
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        auto cmp = [](const vector<int>& lhs, const vector<int>& rhs) {
+            return lhs[0] - lhs[1] < rhs[0] - rhs[1];
+        };
+        sort(costs.begin(), costs.end(), cmp);
+
+        int n = costs.size();
+        int total = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i < n / 2) {
+                total += costs[i][0];
+            } else {
+                total += costs[i][1];
             }
         }
 
-        return dp[n][n / 2];
+        return total;
     }
 };
 
