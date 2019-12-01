39fa6b440a7526270b5f3df110f246b58231abf8
wonter <100wonter@gmail.com>
2019-12-01 20:57:29 +0800

    use iteration istead of recursion

--- 1278.palindrome-partitioning-iii.cpp
+++ 1278.palindrome-partitioning-iii.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     int palindromePartition(string s, int k) {
-        vector<vector<int>> dp(s.size(), vector<int>(k + 1, -1));
-        return dfs(s, dp, 0, k);
+        int dp[s.size() + 1][k + 1];
+        for (int i = 0; i <= s.size(); ++i) {
+            for (int j = 0; j <= k; ++j) {
+                dp[i][j] = s.size();
+            }
+        }
+        dp[0][0] = 0;
+
+        for (int i = 1; i <= s.size(); ++i) {
+            for (int j = 1; j <= k; ++j) {
+                for (int l = 1; l <= i; ++l) {
+                    dp[i][j] = min(dp[i][j], dp[i - l][j - 1] + make_palindromic(s, i - l, i - 1));
+                }
+            }
+        }
+
+        return dp[s.size()][k];
     }
 
-    int dfs(const string& s, vector<vector<int>>& dp, int i, int k) {
-        cout << i << ", " << k << endl;
-        if (s.size() - i < k) {
-            return 1000000;
-        }
-        if (i == s.size() && k == 0) {
-            return 0;
-        }
-        if (dp[i][k] != -1) {
-            return dp[i][k];
-        }
-
-        dp[i][k] = 1000000;
-        for (int l = 1; l <= s.size() - i; ++l) {
-            dp[i][k] = min(dp[i][k], make_palindromic(s, i, i + l) + dfs(s, dp, i + l, k - 1));
-        }
-        return dp[i][k];
-    }
-
-    // [i, j)
     int make_palindromic(const std::string& s, int i, int j) {
-        if (j - i <= 1) {
+        if (i >= j) {
             return 0;
         }
-        if (s[i] == s[j - 1]) {
+        if (s[i] == s[j]) {
             return make_palindromic(s, i + 1, j - 1);
         }
         return make_palindromic(s, i + 1, j - 1) + 1;
